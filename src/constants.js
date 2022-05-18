c9b03cb97c8736184be47ec4b46fcc403754078a
Rohit Singh <brownrbt172@gmail.com>
2022-05-18 12:57:08 +0530

    slack bot

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const SLACK_OAUTH_TOKEN =
+  "xoxb-3529712125026-3543612656308-058I9eLezwD0pSBdz5ADfUZx";
+export const BOT_SPAM_CHANNEL = "#testing"; // this is the channel you want your bot online & spam to go
